2f05933b00c2219bff65336f04efcf0ae7807efb
Hilverton Cesar <44450158+Hilverton@users.noreply.github.com>
2019-09-25 15:41:48 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -29,11 +29,7 @@ app.use((req, res, next) => {
     return next();
 });
 
-app.use(unction(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
